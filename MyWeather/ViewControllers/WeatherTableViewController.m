9758c261c7846d7d6948f57e2ab0153dfe1734fa
Simone Leoni <leonisimone@protonmail.com>
2022-07-08 12:55:39 +0200

    Finished main screen information display

--- MyWeather/ViewControllers/WeatherTableViewController.m
+++ MyWeather/ViewControllers/WeatherTableViewController.m
@@ -48,19 +48,63 @@
     NSLog(@"city: %@", self.city.name);
     NSLog(@"lat: %f", self.city.latitude);
     NSLog(@"long: %f", self.city.longitude);
-    // TODO: add other data and parse values
+
     dispatch_queue_t queue = dispatch_queue_create("get_meteo_information", NULL);
     dispatch_async(queue, ^{
-        NSString *urlString = [NSString stringWithFormat: @"https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current_weather=true", self.city.latitude, self.city.longitude];
+        NSString *urlString = [NSString stringWithFormat: @"https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current_weather=true&daily=temperature_2m_max,temperature_2m_min,weathercode&timezone=UTC", self.city.latitude, self.city.longitude];
         NSURL *url = [NSURL URLWithString:urlString];
         NSData *data = [NSData dataWithContentsOfURL:url];
         NSLog(@"%@", data.description);
         id value = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
         NSDictionary *weather = (NSDictionary *)value;
-        NSString *current_weather = [weather valueForKey:@"current_weather"];
+        // get current weather informations
+        NSDictionary *current_weather = [weather valueForKey:@"current_weather"];
+        NSNumber *temp = [current_weather valueForKey:@"temperature"];
+        NSNumber *windspeed = [current_weather valueForKey:@"windspeed"];
+        NSNumber *winddirection = [current_weather valueForKey:@"winddirection"];
+        NSString *direction;
+        NSNumber *weathercode = [current_weather valueForKey:@"weathercode"];
+        // set wind direction
+        if((winddirection.intValue >= 0 && winddirection.intValue < 23) || (winddirection.intValue > 337 && winddirection.intValue <= 360))
+            direction = @"N";
+        else if(winddirection.intValue >= 23 && winddirection.intValue < 67)
+            direction = @"NE";
+        else if(winddirection.intValue >= 67 && winddirection.intValue < 113)
+            direction = @"E";
+        else if(winddirection.intValue >= 113 && winddirection.intValue < 158)
+            direction = @"SE";
+        else if(winddirection.intValue >= 158 && winddirection.intValue < 203)
+            direction = @"S";
+        else if(winddirection.intValue >= 203 && winddirection.intValue < 248)
+            direction = @"SO";
+        else if(winddirection.intValue >= 248 && winddirection.intValue < 293)
+            direction = @"O";
+        else
+            direction = @"NO";
+        
+        // daily informations
+        NSDictionary *daily = [weather valueForKey:@"daily"];
+        NSArray *temperature_min = [daily valueForKey:@"temperature_2m_min"];
+        NSArray *temperature_max = [daily valueForKey:@"temperature_2m_max"];
+        NSArray *weathercond = [daily valueForKey:@"weathercode"];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.TemperatureCell.textLabel.text = [NSString stringWithFormat:@"%d °C", temp.intValue];
+            self.WindSpeedCell.detailTextLabel.text = [NSString stringWithFormat:@"%d Km/h", windspeed.intValue];
+            self.WindDirectionCell.detailTextLabel.text = direction;
+            self.ConditionImage.image = [self imageForWeatherCode:weathercode.intValue];
+            self.ConditionCell.textLabel.text = [self stringForWeatherCode:weathercode.intValue];
+            self.FirstDayCell.imageView.image = [self imageForWeatherCode:((NSNumber *)[weathercond objectAtIndex:1]).intValue];
+            self.FirstDayCell.textLabel.text = [NSString stringWithFormat:@"%d/%d", ((NSNumber *)[temperature_min objectAtIndex:1]).intValue, ((NSNumber *)[temperature_max objectAtIndex:1]).intValue];
+            self.SecondDayCell.imageView.image = [self imageForWeatherCode:((NSNumber *)[weathercond objectAtIndex:2]).intValue];
+            self.SecondDayCell.textLabel.text = [NSString stringWithFormat:@"%d/%d", ((NSNumber *)[temperature_min objectAtIndex:2]).intValue, ((NSNumber *)[temperature_max objectAtIndex:2]).intValue];
+            self.ThirdDayCell.imageView.image = [self imageForWeatherCode:((NSNumber *)[weathercond objectAtIndex:3]).intValue];
+            self.ThirdDayCell.textLabel.text = [NSString stringWithFormat:@"%d/%d", ((NSNumber *)[temperature_min objectAtIndex:3]).intValue, ((NSNumber *)[temperature_max objectAtIndex:3]).intValue];
+            self.ForthDayCell.imageView.image = [self imageForWeatherCode:((NSNumber *)[weathercond objectAtIndex:4]).intValue];
+            self.ForthDayCell.textLabel.text = [NSString stringWithFormat:@"%d/%d", ((NSNumber *)[temperature_min objectAtIndex:4]).intValue, ((NSNumber *)[temperature_max objectAtIndex:4]).intValue];
+        });
     });
 }
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -108,6 +152,67 @@
 
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
     NSLog(@"error %@", error.domain);
+}
+
+-(UIImage *)imageForWeatherCode:(int) value {
+    // wmo weather interpretation codes
+    UIImage *result;
+    if(value == 0)
+        return [UIImage systemImageNamed:@"sun.max"];
+    if(value == 1 || value == 2 || value == 3)
+        return [UIImage systemImageNamed:@"cloud.sun"];
+    if(value == 45 || value == 48)
+        return [UIImage systemImageNamed:@"cloud.fog"];
+    if(value == 51 || value == 53 || value == 55)
+        return [UIImage systemImageNamed:@"cloud.drizzle"];
+    if(value == 56 || value == 57)
+        return [UIImage systemImageNamed:@"snow"];
+    if(value == 61 || value == 63 || value == 65)
+        return [UIImage systemImageNamed:@"cloud.rain"];
+    if(value == 66 || value == 67)
+        return [UIImage systemImageNamed:@"cloud.rain"];
+    if(value == 71 || value == 73 || value == 75)
+        return [UIImage systemImageNamed:@"cloud.snow"];
+    if(value == 77)
+        return [UIImage systemImageNamed:@"cloud.snow"];
+    if(value == 80 || value == 81 || value == 82)
+        return [UIImage systemImageNamed:@"cloud.rain"];
+    if(value == 85 || value == 86)
+        return [UIImage systemImageNamed:@"cloud.snow"];
+    if(value == 95 || value == 96 || value == 99)
+        return [UIImage systemImageNamed:@"cloud.bolt"];
+    else
+        return nil;
+}
+
+-(NSString *)stringForWeatherCode:(int)value {
+    // wmo weather interpretation codes
+    if(value == 0)
+        return @"Soleggiato";
+    if(value == 1 || value == 2 || value == 3)
+        return @"Parzialmente nuovoloso";
+    if(value == 45 || value == 48)
+        return @"Nebbia";
+    if(value == 51 || value == 53 || value == 55)
+        return @"Rovesci sparsi";
+    if(value == 56 || value == 57)
+        return @"Nevischio";
+    if(value == 61 || value == 63 || value == 65)
+        return @"Pioggia";
+    if(value == 66 || value == 67)
+        return @"Pioggia";
+    if(value == 71 || value == 73 || value == 75)
+        return @"Neve";
+    if(value == 77)
+        return @"Nevicate intense";
+    if(value == 80 || value == 81 || value == 82)
+        return @"Acquazzone";
+    if(value == 85 || value == 86)
+        return @"Nevicate intense";
+    if(value == 95 || value == 96 || value == 99)
+        return @"Tempesta";
+    else
+        return @"";
 }
 
 #pragma mark - Navigation
